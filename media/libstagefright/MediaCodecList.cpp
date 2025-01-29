8553eeacb00e43e6e0ab7c799293e5469c95a2b5
John Macka <macka2004@gmail.com>
2025-01-29 12:06:18 +1000

    Revert "libstagefright: omx: Add support for loading prebuilt ddp and ac4 decoder lib"
    This reverts commit 4c10cf16b6a23f6379b1f5d3c604fddf69344979.

--- media/libstagefright/MediaCodecList.cpp
+++ media/libstagefright/MediaCodecList.cpp
@@ -319,7 +319,6 @@ const sp<AMessage> MediaCodecList::getGlobalSettings() const {
 //static
 bool MediaCodecList::isSoftwareCodec(const AString &componentName) {
     return componentName.startsWithIgnoreCase("OMX.google.")
-            || componentName.startsWithIgnoreCase("OMX.dolby.")
             || componentName.startsWithIgnoreCase("c2.android.")
             || (!componentName.startsWithIgnoreCase("OMX.")
                     && !componentName.startsWithIgnoreCase("c2."));
